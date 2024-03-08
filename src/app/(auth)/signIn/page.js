467192104967a3672bc6c4bd773d9f1712713bb1
Antonio <contact@goweki.com>
2024-03-08 18:45:27 +0300

    trans-night

--- src/app/(auth)/signIn/page.js
+++ src/app/(auth)/signIn/page.js
@@ -78,7 +78,7 @@ export default function SigninPage(params) {
           </div>
 
           <div class="mt-3 text-xs flex justify-between items-center text-[#002D74]">
-            <p>Don't have an account?</p>
+            <p>Don&apos;t have an account?</p>
             <button class="py-2 px-5 bg-white border rounded-xl hover:scale-110 duration-300">
               Register
             </button>
