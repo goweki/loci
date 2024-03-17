5b197aa14237bc18be6c7d3857e1fb542afe25a7
Antonio <contact@goweki.com>
2024-03-17 06:03:57 +0300

    init

--- src/components/atoms/inputs.js
+++ src/components/atoms/inputs.js
@@ -1,3 +1,4 @@
+"use client";
 export function InputField({
   label,
   name = "inputField",
--- src/components/elements/footer.js
+++ src/components/elements/footer.js
@@ -1,163 +1,41 @@
+"use client";
+import { useState } from "react";
 import Link from "next/link";
+import { LogoSymbol } from "../atoms/svgs";
+import { InputField } from "../atoms/inputs";
+import { ButtonPrimary } from "../atoms/buttons";
 
 export default function Footer() {
+  const [email, setEmail] = useState("");
   return (
     <footer id="footer">
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
         {/* Top area: Blocks */}
-        <div className="grid sm:grid-cols-12 gap-8 py-8 md:py-12 border-t border-gray-200">
+        <div className="grid sm:grid-cols-6 gap-8 py-8 md:py-12 border-t border-gray-200">
           {/* 1st block */}
-          <div className="sm:col-span-12 lg:col-span-3">
+          <div className="sm:col-span-6">
             <div className="mb-2">
               {/* Logo */}
-              <Link href="/" className="inline-block" aria-label="Cruip">
-                <svg
-                  className="w-8 h-8"
-                  viewBox="0 0 32 32"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <defs>
-                    <radialGradient
-                      cx="21.152%"
-                      cy="86.063%"
-                      fx="21.152%"
-                      fy="86.063%"
-                      r="79.941%"
-                      id="footer-logo"
-                    >
-                      <stop stopColor="#4FD1C5" offset="0%" />
-                      <stop stopColor="#81E6D9" offset="25.871%" />
-                      <stop stopColor="#338CF5" offset="100%" />
-                    </radialGradient>
-                  </defs>
-                  <rect
-                    width="32"
-                    height="32"
-                    rx="16"
-                    fill="url(#footer-logo)"
-                    fillRule="nonzero"
-                  />
-                </svg>
-              </Link>
-            </div>
-            <div className="text-sm text-gray-600">
               <Link
-                href="#"
-                className="text-gray-600 hover:text-gray-900 hover:underline transition duration-150 ease-in-out"
+                href="/"
+                className="group inline-flex items-center border-b pb-3 border-slate-200"
               >
-                Terms
-              </Link>{" "}
-              ·{" "}
-              <Link
-                href="#"
-                className="text-gray-600 hover:text-gray-900 hover:underline transition duration-150 ease-in-out"
-              >
-                Privacy Policy
+                <LogoSymbol classname="w-10 h-10 group-hover:scale-110 transition-all" />
+
+                <span className="md:inline-block mx-2 font-semibold text-sky-700">
+                  LOCi
+                </span>
               </Link>
             </div>
           </div>
 
-          {/* 2nd block */}
-          <div className="sm:col-span-6 md:col-span-3 lg:col-span-2">
-            <h6 className="text-gray-800 font-medium mb-2">Products</h6>
+          {/* Quick links block */}
+          <div className="sm:col-span-6 md:col-span-3 ">
+            <h6 className="text-gray-800 font-medium mb-2">Quick links</h6>
             <ul className="text-sm">
               <li className="mb-2">
                 <Link
-                  href="#"
-                  className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out"
-                >
-                  Web Studio
-                </Link>
-              </li>
-              <li className="mb-2">
-                <Link
-                  href="#"
-                  className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out"
-                >
-                  DynamicBox Flex
-                </Link>
-              </li>
-              <li className="mb-2">
-                <Link
-                  href="#"
-                  className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out"
-                >
-                  Programming Forms
-                </Link>
-              </li>
-              <li className="mb-2">
-                <Link
-                  href="#"
-                  className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out"
-                >
-                  Integrations
-                </Link>
-              </li>
-              <li className="mb-2">
-                <Link
-                  href="#"
-                  className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out"
-                >
-                  Command-line
-                </Link>
-              </li>
-            </ul>
-          </div>
-
-          {/* 3rd block */}
-          <div className="sm:col-span-6 md:col-span-3 lg:col-span-2">
-            <h6 className="text-gray-800 font-medium mb-2">Resources</h6>
-            <ul className="text-sm">
-              <li className="mb-2">
-                <Link
-                  href="#"
-                  className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out"
-                >
-                  Documentation
-                </Link>
-              </li>
-              <li className="mb-2">
-                <Link
-                  href="#"
-                  className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out"
-                >
-                  Tutorials & Guides
-                </Link>
-              </li>
-              <li className="mb-2">
-                <Link
-                  href="#"
-                  className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out"
-                >
-                  Blog
-                </Link>
-              </li>
-              <li className="mb-2">
-                <Link
-                  href="#"
-                  className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out"
-                >
-                  Support Center
-                </Link>
-              </li>
-              <li className="mb-2">
-                <Link
-                  href="#"
-                  className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out"
-                >
-                  Partners
-                </Link>
-              </li>
-            </ul>
-          </div>
-
-          {/* 4th block */}
-          <div className="sm:col-span-6 md:col-span-3 lg:col-span-2">
-            <h6 className="text-gray-800 font-medium mb-2">Company</h6>
-            <ul className="text-sm">
-              <li className="mb-2">
-                <Link
-                  href="#"
+                  href="/"
                   className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out"
                 >
                   Home
@@ -165,82 +43,49 @@ export default function Footer() {
               </li>
               <li className="mb-2">
                 <Link
-                  href="#"
+                  href="/blog"
                   className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out"
                 >
-                  About us
+                  Blog
                 </Link>
               </li>
               <li className="mb-2">
                 <Link
-                  href="#"
+                  href="/contacts"
                   className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out"
                 >
-                  Company values
-                </Link>
-              </li>
-              <li className="mb-2">
-                <Link
-                  href="#"
-                  className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out"
-                >
-                  Pricing
-                </Link>
-              </li>
-              <li className="mb-2">
-                <Link
-                  href="#"
-                  className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out"
-                >
-                  Privacy Policy
+                  Contacts
                 </Link>
               </li>
             </ul>
           </div>
 
-          {/* 5th block */}
-          <div className="sm:col-span-6 md:col-span-3 lg:col-span-3">
-            <h6 className="text-gray-800 font-medium mb-2">Subscribe</h6>
+          {/* Request demo block */}
+          <div className="sm:col-span-6 md:col-span-3">
+            <h6 className="text-gray-800 font-medium mb-2">Request Demo</h6>
             <p className="text-sm text-gray-600 mb-4">
-              Get the latest news and articles to your inbox every month.
+              Leave us with your email and we will be in touch within a week to
+              set up a demo
             </p>
             <form>
-              <div className="flex flex-wrap mb-4">
-                <div className="w-full">
-                  <label className="block text-sm sr-only" htmlFor="newsletter">
-                    Email
-                  </label>
-                  <div className="relative flex items-center max-w-xs">
-                    <input
-                      id="newsletter"
-                      type="email"
-                      className="form-input w-full text-gray-800 px-3 py-2 pr-12 text-sm"
-                      placeholder="Your email"
-                      required
-                    />
-                    <button
-                      type="submit"
-                      className="absolute inset-0 left-auto"
-                      aria-label="Subscribe"
+              <div className="grid grid-cols-4 space-x-2 ">
+                <div className="col-span-3">
+                  <InputField name="email" />
+                </div>
+                <div className="col-span-1">
+                  <button
+                    className="rounded-lg w-full h-full bg-sky-600 disabled:opacity-25 enabled:hover:bg-sky-700"
+                    disabled={!!email}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      fill="currentColor"
+                      className="w-6 h-6 m-auto text-white"
                     >
-                      <span
-                        className="absolute inset-0 right-auto w-px -ml-px my-2 bg-gray-300"
-                        aria-hidden="true"
-                      ></span>
-                      <svg
-                        className="w-3 h-3 fill-current text-blue-600 mx-3 flex-shrink-0"
-                        viewBox="0 0 12 12"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z"
-                          fillRule="nonzero"
-                        />
-                      </svg>
-                    </button>
-                  </div>
-                  {/* Success message */}
-                  {/* <p className="mt-2 text-green-600 text-sm">Thanks for subscribing!</p> */}
+                      <path d="M3.478 2.404a.75.75 0 0 0-.926.941l2.432 7.905H13.5a.75.75 0 0 1 0 1.5H4.984l-2.432 7.905a.75.75 0 0 0 .926.94 60.519 60.519 0 0 0 18.445-8.986.75.75 0 0 0 0-1.218A60.517 60.517 0 0 0 3.478 2.404Z" />
+                    </svg>
+                  </button>
                 </div>
               </div>
             </form>
