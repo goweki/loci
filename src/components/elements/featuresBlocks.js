c5112b809a1c1e07850fbef48e917815522c9bef
Antonio <contact@goweki.com>
2024-03-17 18:47:21 +0300

    ...

--- src/components/elements/featuresBlocks.js
+++ src/components/elements/featuresBlocks.js
@@ -8,7 +8,7 @@ export default function FeaturesBlocks() {
       ></div>
       {/* <div className="absolute left-0 right-0 bottom-0 m-auto w-px p-px h-20 bg-gray-200 transform translate-y-1/2"></div> */}
 
-      <div className="relative max-w-6xl mx-auto px-4 sm:px-6">
+      <div className="relative max-w-screen-xl mx-auto px-4 sm:px-6">
         <div className="py-12 md:py-20">
           {/* Section header */}
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
