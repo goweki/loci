b6d4e16a9b56296313e8e64a5fa66eb3c554f1d9
Antonio <contact@goweki.com>
2024-03-18 17:59:09 +0300

    named anonymous default export function

--- src/components/mols/Breadcrumb.js
+++ src/components/mols/Breadcrumb.js
@@ -1,6 +1,6 @@
 import Link from "next/link";
 
-export default function ({ pageName }) {
+export default function BreadCrumb({ pageName }) {
   return (
     <div className="mb-6 flex flex-col gap-3 sm:flex-row sm:items-center sm:justify-between">
       <h2 className="text-title-md2 font-semibold text-black dark:text-white">
