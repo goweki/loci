d1df37cbfb8fdb6c835489a64052ff305f481dbc
Goweki <contact@goweki.com>
2025-11-05 14:49:58 +0300

    metadata update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,12 +3,18 @@ export const metadata = {
     template: "%s | loci ",
     default: "loci",
   },
-  description: "Supercharging Business Communications",
+  description: "Supercharging Engagement",
   metadataBase: new URL(
     process.env.NODE_ENV === "development"
       ? "http://localhost:3000"
       : process.env.NEXTAUTH_URL
   ),
+  icons: {
+    icon: "/brand/favicon.ico",
+    shortcut: "/brand/favicon-16x16.png",
+    apple: "/brand/apple-touch-icon.png",
+  },
+  manifest: "/brand/site.webmanifest",
   keywords: [
     "Communication",
     "WhatsApp",
@@ -19,6 +25,27 @@ export const metadata = {
   robots: {
     index: false,
     follow: false,
+  },
+
+  openGraph: {
+    title: "loci",
+    description: "Supercharging Engagement",
+    url:
+      process.env.NODE_ENV === "development"
+        ? "http://localhost:3000"
+        : process.env.NEXTAUTH_URL,
+    siteName: "loci",
+    images: [
+      {
+        url: "/brand/og_image.png",
+        width: 1200,
+        height: 630,
+        alt: "loci - Supercharging Engagement", // Alternative text for accessibility
+      },
+      // You can add more images here for different sizes/formats
+    ],
+    locale: "en_US",
+    type: "website",
   },
 };
 
