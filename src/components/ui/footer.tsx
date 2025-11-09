5ea8f6ee40f955995c7339b56525452014b85182
Goweki <contact@goweki.com>
2025-11-09 13:13:01 +0300

    updates footer links

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -1,26 +1,35 @@
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Separator } from "@/components/ui/separator";
+"use client";
+
 import { SocialIcon } from "react-social-icons";
 import Link from "next/link";
 import { BrandSymbol } from "./brand";
-import { Label } from "./label";
+import { useI18n } from "@/lib/i18n";
 
-const footerLinks = [
-  {
-    title: "Help",
-    href: "/contact-us",
-  },
-  {
-    title: "Privacy",
-    href: "/privacy",
-  },
-];
+const Footer = () => {
+  const { language } = useI18n();
+  const footerLinks = [
+    {
+      title: "Privacy policy",
+      href: `/${language}/privacy-policy`,
+    },
+  ];
 
 const Footer = () => {
   return (
     <footer className="border-t mt-8">
       <div className="max-w-screen-xl mx-auto">
+        <div className="px-6 text-muted-foreground flex flex-col justify-between gap-4 pt-8 text-sm font-medium md:flex-row md:items-center">
+          <div className="h-6 w-6">
+            <BrandSymbol />
+          </div>
+          <ul className="flex gap-4">
+            {footerLinks.map((link, linkIdx) => (
+              <li key={linkIdx} className="hover:text-primary underline">
+                <a href={link.href}>{link.title}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
         <div className="py-8 flex flex-col-reverse sm:flex-row items-center justify-between gap-x-2 gap-y-5 px-6">
           <span className="text-muted-foreground">
             &copy; {new Date().getFullYear()}{" "}
