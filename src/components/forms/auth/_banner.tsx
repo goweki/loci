f149d0a935386d6f268b2726e179573b2b1f76df
Goweki <contact@goweki.com>
2026-03-26 16:49:47 +0300

    ...

--- src/components/forms/auth/_banner.tsx
+++ src/components/forms/auth/_banner.tsx
@@ -3,8 +3,6 @@
 import { SocialIcon } from "react-social-icons";
 
 export function SideBanner() {
-  const sm_links: string[] = ["https://www.linkedin.com/in/goweki"];
-
   return (
     <div className="flex flex-row space-x-4 items-center">
       <div className="pt-1 text-sm">
@@ -18,16 +16,6 @@ export function SideBanner() {
           KOMOLO
         </a>
       </div>
-      {sm_links.map((link, i) => (
-        <span key={i} className="h-6 w-6">
-          <SocialIcon
-            url={link}
-            style={{ height: "100%", width: "100%" }}
-            target="_blank"
-            rel="noopener noreferrer"
-          />
-        </span>
-      ))}
     </div>
   );
 }
